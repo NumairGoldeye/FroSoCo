204483f0a3b1a5fce660f8465b1a2f4ef99a5734
NumairGoldeye <flamewingman28@gmail.com>
2014-01-08 17:28:50 -0800

    Added Samples
    Added sample data and output

---
+++ sample_output.py
@@ -0,0 +1,62 @@
+Round  1
+enrolling Khalil Griffin in The FroSoCo Mural [Sat 10:00am - 11:00am]
+enrolling Fake Man in Calligraphy [Sun 4:00pm - 5:00pm]
+enrolling Fake Woman in Calligraphy [Sun 4:00pm - 5:00pm]
+
+Round  2
+enrolling Khalil Griffin in Web Development [Mon 7:00pm - 8:00pm]
+enrolling Fake Child in Calligraphy [Sun 4:00pm - 5:00pm]
+
+Round  3
+enrolling Fake Man in Yaoi to Yuri [Thurs 7:30pm - 9:30pm]
+
+Round  4
+enrolling Fake Child in Yaoi to Yuri [Thurs 7:30pm - 9:30pm]
+
+Round  5
+enrolling Fake Child in J.R.R. Tolkien [Tues 7:00pm - 9:00pm]
+
+Round  6
+enrolling Fake Child in Rocket Science [Thurs 6:30pm - 8:30pm]
+
+Students:
+Fake Woman
+Calligraphy [Sun 4:00pm - 5:00pm]
+
+Khalil Griffin
+Web Development [Mon 7:00pm - 8:00pm]
+The FroSoCo Mural [Sat 10:00am - 11:00am]
+
+Fake Man
+Calligraphy [Sun 4:00pm - 5:00pm]
+Yaoi to Yuri [Thurs 7:30pm - 9:30pm]
+
+Fake Child
+Rocket Science [Thurs 6:30pm - 8:30pm]
+J.R.R. Tolkien [Tues 7:00pm - 9:00pm]
+Calligraphy [Sun 4:00pm - 5:00pm]
+Yaoi to Yuri [Thurs 7:30pm - 9:30pm]
+
+____________
+Classes:
+The FroSoCo Mural [Sat 10:00am - 11:00am] [1 enrolled]
+Khalil Griffin
+
+Web Development [Mon 7:00pm - 8:00pm] [1 enrolled]
+Khalil Griffin
+
+Calligraphy [Sun 4:00pm - 5:00pm] [3 enrolled]
+Fake Man
+Fake Child
+Fake Woman
+
+Rocket Science [Thurs 6:30pm - 8:30pm] [1 enrolled]
+Fake Child
+
+Yaoi to Yuri [Thurs 7:30pm - 9:30pm] [2 enrolled]
+Fake Man
+Fake Child
+
+J.R.R. Tolkien [Tues 7:00pm - 9:00pm] [1 enrolled]
+Fake Child
+
